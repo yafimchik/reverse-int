25debc75169a0fed29adb8083b7bdb3d3c21fad9
yafimchik <yafimenka.jk@gmail.com>
2020-03-08 11:14:12 +0300

    init: solve task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let reverseN = 0;
+    while (n > 0){
+        reverseN = reverseN * 10 + (n % 10);
+        n = Math.trunc(n / 10);
+    }
+    return reverseN;
 }
